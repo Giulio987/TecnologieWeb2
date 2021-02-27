594b6940f80bf9792c5f7dbf79ccd879809b1bf5
Giuseppe Giacalone <giuseppe.giacalone@edu.unife.it>
2021-02-27 19:28:19 +0100

    edit doctor

--- resources/views/doctor/edit.blade.php
+++ resources/views/doctor/edit.blade.php
@@ -2,9 +2,26 @@
 
 @section('content')
 
-<div class="container">
-    <h1> Modifica Le Informazioni Sul Dottore</h1>
-
+<div class="row-space" style="margin-left:100px;float:left;">
+<a href="{{ URL::action('DoctorController@index') }}">
+<button style="background-color: #f8fafc;border-width: 0px;" href="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+    </svg>
+  </button>
+</a>
+</div>
+<div class="container-lg" align="center">
+<div class="row row-space justify-content-center">
+        <h1 class="font-weight-bold">
+            sssss
+        </h1>
+    </div>
+    <div class="row row-space justify-content-center">
+        <h5>
+            Comdddddddddddddddddd.<button type="submit" name="submit" class="btn btn-outline-success btn-prenota font-weight-bold col-lg-2">+</button>
+        </h5>
+    </div>
     <form action="{{ URL::action('DoctorController@update', $doctor) }}" method="POST">
         <input type="hidden" name="_method" value="PATCH">
         {{ csrf_field() }}
@@ -17,57 +34,72 @@
                 $email = $info->email;
             }  
         ?>  
+    <div class="row row-space justify-content-center border-form align-items-center">
+
+    <div class="col-lg-5">
+        <div class="row row-space justify-content-center">
         <div class="form-group">
-            <label for="name">Nome</label>
             <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $nome }}">
             <small class="form-text text-muted">Modifica il nome del dottore</small>
             @error('name')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
+        </div>
+
+        <div class="row row-space justify-content-center">
         <div class="form-group">
-          <label for="surname">Cognome</label>
           <input type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ $cognome }}">
           <small class="form-text text-muted">Modifica il cognome del dottore</small>
             @error('surname')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
+        </div>
+
+        <div class="row row-space justify-content-center">
         <div class="form-group">
-            <label for="fiscal_code">Codice Fiscale</label>
             <input type="text" class="form-control @error('fiscal_code') is-invalid @enderror" name="fiscal_code" value="{{ $doctor->fiscal_code }}">
             <small class="form-text text-muted">Modifica il codice fiscale del dottore</small>
             @error('fiscal_code')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
+        </div>
+        </div>
+        <div class="col-lg-5">
+        <div class="row row-space justify-content-center">
         <div class="form-group">
-            <label for="gender">Sesso</label>
             <input type="text" class="form-control @error('gender') is-invalid @enderror" name="gender" value="{{ $doctor->gender }}">
             <small class="form-text text-muted">Modifica il sesso del dottore</small>
             @error('gender')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
+        </div>
+
+        <div class="row row-space justify-content-center">
         <div class="form-group">
-            <label for="dob">Data Di Nascita</label>
             <input type="date" class="form-control @error('dob') is-invalid @enderror" name="dob" value="{{ $doctor->dob }}">
             <small class="form-text text-muted">Modifica la data di nascita del dottore</small>
             @error('dob')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
+        </div>
+        <div class="row row-space justify-content-center">
         <div class="form-group">
-            <label for="phone_number">Telefono</label>
             <input type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ $doctor->phone_number }}">
             <small class="form-text text-muted">Modifica il telefono del dottore</small>
             @error('phone_number')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="form-group">
-            <label for="id_building">Id Edificio</label>
-            <select name="id_building" id="building">
+        </div>
+        </div>
+        <div class="row  justify-content-center" align="center">
+        <div class="form-group"> 
+            <select name="id_building" id="selUser">
                 @foreach($buildings as $b)
                     <option value="{{$b->id}}">{{$b->id}} - {{$b->street_address }} - {{$b->street_number}} - {{$b->postal_code}} - {{$b->city}}</option>
                 @endforeach
@@ -77,13 +109,11 @@
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
-        
-        <button type="submit" class="btn btn-primary">Aggiorna</button>
-
-        <a href="{{ URL::action('DoctorController@index') }}" class="btn btn-secondary">Indietro</a>
+        </div>
 
     </form>    
 </div>
+        </div>
 
 <script type="application/javascript">
         // Example starter JavaScript for disabling form submissions if there are invalid fields
