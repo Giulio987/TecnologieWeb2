0417213d1ccbfc922f39607e5af6cc208169c116
Giuseppe Giacalone <giuseppe.giacalone@edu.unife.it>
2021-02-28 12:23:31 +0100

    notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,17 +75,34 @@
                             </div>
                             
                         </li>
-                        <li class="nav-item dropdown fill ">
-                            <a class="dropdown-item" href="{{ url('/home') }}">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/home') }}">
                                 Home
                             </a>
                         </li>
                         @endguest
+                        <?php $notification = count(DB::table('prescriptions')->where('status', 'convalidare')->get()); ?>
+
                         <li class="nav-item"><a class="nav-link" href="/#why-us">Area Personale</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/#why-us"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                        @if($notification > 0)
+                        <li class="nav-item dropdown fill ">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
                                 <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
                             </svg>
-                            <span class="badge notification align-items-start">{{ $notification }}</span></a></li>
+                            <span class="badge notification align-items-start">{{ $notification }}</span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ URL::action('PrescriptionController@indexValidate') }}">
+                                @if($notification == 1)
+                                    Hai {{ $notification }} prescrizione da convalidare
+                                @else 
+                                    Hai {{ $notification }} prescrizioni da convalidare
+                                @endif
+                            </a>
+                            </div>
+                        </li>
+                        @endif
                         <li class="nav-item"><a class="nav-link" href="/#why-us">Perchè</a></li>
                         <li class="nav-item"><a class="nav-link" href="/#servizi">Servizi</a></li>
                         <li class="nav-item"><a class="nav-link" href="/#dottori">Dottori</a></li>
