667d910cf389898cd5c5603f1bdd0fcba9a8c8b8
Giuseppe Giacalone <giuseppe.giacalone@edu.unife.it>
2021-02-28 22:52:06 +0100

    modifiche

--- resources/views/patient/create.blade.php
+++ resources/views/patient/create.blade.php
@@ -36,7 +36,7 @@ $name = Auth::user()->name;
 
     <div class="row row-space justify-content-center">
         <h5>
-            Compila tutti i campi e crea il tuo nuovo paziente.<button type="submit" name="submit" class="btn btn-outline-success btn-prenota font-weight-bold col-lg-2">+</button>
+            Compila tutti i campi e crea il tuo nuovo paziente.
         </h5>
     </div>
 
@@ -151,6 +151,10 @@ $name = Auth::user()->name;
                 </div>
 </div>
 </div>
+<button class="btn btn-outline-success col-lg-2">Registra<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus-circle pl-2" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg></button>
 </div>       
 
             </form>
