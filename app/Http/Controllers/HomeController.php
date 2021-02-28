60f6c56e3df65a895dc5aa0230933eecdc47ee0f
RosariaCiociola <rosaria.ciociola@edu.unife.it>
2021-02-28 14:10:21 +0100

    area personale terminata

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use DB;
 use App\Doctor;
 use App\User;
 use App\Patient;
+use App\Building;
 
 
 class HomeController extends Controller
@@ -43,18 +44,19 @@ class HomeController extends Controller
         else if ( Auth::user()->role == '2') {
             //prendo i dati del dottore loggato
             $id = Auth::user()->id;
+            $user = DB::table('users')->where('id', $id)->first();
             $doctor = DB::table('doctors')->where('id_user', $id)->first();
-            return view('personal_area.index', compact('doctor'));
+            $patients = Patient::where('id_doctor', '=', $doctor->id)->count();
+            $building = Building::where('id', '=', $doctor->id_building)->first();
+            return view('personal_area.index', compact('user', 'doctor', 'building', 'patients'));
         }else if ( Auth::user()->role == '3') {
             //prendo i dati del paziente loggato e del suo medico curante
-            $id = Auth::user()->id;
-            $patient = DB::table('patients')->where('id_user', $id)->get();
-            foreach($patient as $p)
-            {
-                $res = $p->id_doctor;
-            }
-            $doctor = DB::table('doctors')->where('id', $res)->get();
-            return view('personal_area.index', compact('patient', 'doctor'));
+            $user = DB::table('users')->where('id', Auth::user()->id)->first();
+            $patient = DB::table('patients')->where('id_user', $user->id)->first();
+            $doctor = DB::table('doctors')->where('id', $patient->id_doctor)->first();
+            $userDoctor = DB::table('users')->where('id', $doctor->id_user)->first();
+            $building = Building::where('id', '=', $doctor->id_building)->first();
+            return view('personal_area.index', compact('user', 'patient', 'doctor', 'userDoctor', 'building'));
         }
     }
 }
--- resources/views/personal_area/index.blade.php
+++ resources/views/personal_area/index.blade.php
@@ -4,15 +4,6 @@
 
 
 @if(!strcmp(Auth::user()->role, '1'))
-{{$admin->fiscal_code}} admin
-
-@endif
-
-@if(!strcmp(Auth::user()->role, '2'))
-
-<?php
-$name = Auth::user()->name;
-?>
 
 <div class="row-space" style="margin-left:100px;float:left;">
 <a href="{{ URL::action('DoctorController@index') }}">
@@ -26,7 +17,67 @@ $name = Auth::user()->name;
 <div class="container-lg" align="center">
     <div class="row row-space justify-content-center">
         <h1 class="font-weight-bold">
-            Ciao Dott. {{ $name }}, ti presento la tua area personale.
+            Ciao Dott. {{ $admin->name }}, ti presento la tua area personale.
+        </h1>
+    </div>
+    <div class="row row-space justify-content-center">
+        <h5>
+            Visualizza le tue informazioni e/o modificale.
+        </h5>
+    </div>
+    
+    <div class="row row-space justify-content-center border-form align-items-center">
+
+
+    <div class="col-lg-4">
+        <div class="row row-space justify-content-center">
+
+        <p class="text-uppercase font-weight-bold">Nome</p>
+
+        </div>
+        <div class="row row-space justify-content-center">
+
+        <p class="text-uppercase font-weight-bold">Cognome</p>
+
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Email</p>
+   
+        </div>
+    </div>
+
+            
+    <div class="col-lg-4" align="center">
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $admin->name }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $admin->surname }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $admin->email }}</p>
+        </div>
+    </div>
+</div>
+</div>   
+
+@endif
+
+@if(!strcmp(Auth::user()->role, '2'))
+
+<div class="row-space" style="margin-left:100px;float:left;">
+<a href="{{ URL::action('DoctorController@index') }}">
+<button style="background-color: #f8fafc;border-width: 0px;" href="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+    </svg>
+  </button>
+</a>
+</div>
+<div class="container-lg" align="center">
+    <div class="row row-space justify-content-center">
+        <h1 class="font-weight-bold">
+            Ciao Dott. {{ $user->name }}, ti presento la tua area personale.
         </h1>
     </div>
     <div class="row row-space justify-content-center">
@@ -70,7 +121,7 @@ $name = Auth::user()->name;
                 
         </div>
         <div class="row row-space justify-content-center">
-        <p class="text-uppercase font-weight-bold">Edificio ambulatorio</p>
+        <p class="text-uppercase font-weight-bold">Ambulatorio</p>
 
                 
         </div>
@@ -79,7 +130,7 @@ $name = Auth::user()->name;
 
                 
         </div>
-            </div>
+    </div>
 
             
     <div class="col-lg-4" align="center">
@@ -87,31 +138,30 @@ $name = Auth::user()->name;
             <p class="text-uppercase">{{ $doctor->fiscal_code }}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ $user->name }}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ $user->surname }}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ $user->email }}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ $doctor->gender}}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ date('d/m/Y', strtotime($doctor->dob)) }}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ $doctor->phone_number }}</p>
+        </div>
+        <div class="row row-space justify-content-center"> 
+            <p class="text-uppercase">{{ $building->street_address }}, {{ $building->street_number }} - {{ $building->city }} - {{ $building->postal_code }}</p>
         </div>
         <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
+            <p class="text-uppercase">{{ $patients }}</p>
         </div>
-        <div class="row row-space justify-content-center">    
-            <p class="text-uppercase">inserisci</p>
-        </div>
-
-</div>
+    </div>
 </div>
 </div>       
 
@@ -119,6 +169,121 @@ $name = Auth::user()->name;
 @endif
 @if(!strcmp(Auth::user()->role, '3'))
 
+<div class="row-space" style="margin-left:100px;float:left;">
+<a href="{{ URL::action('DoctorController@index') }}">
+<button style="background-color: #f8fafc;border-width: 0px;" href="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
+    </svg>
+  </button>
+</a>
+</div>
+<div class="container-lg" align="center">
+    <div class="row row-space justify-content-center">
+        <h1 class="font-weight-bold">
+            Ciao Dott. {{ $user->name }}, ti presento la tua area personale.
+        </h1>
+    </div>
+    <div class="row row-space justify-content-center">
+        <h5>
+            Visualizza le tue informazioni e/o modificale.
+        </h5>
+    </div>
+    
+    <div class="row row-space justify-content-center border-form align-items-center">
+
+
+    <div class="col-lg-4">
+        <div class="row row-space justify-content-center">
+                <p class="text-uppercase font-weight-bold">Codice Fiscale</p>
+        </div>
+        <div class="row row-space justify-content-center">
+
+        <p class="text-uppercase font-weight-bold">Nome</p>
+
+        </div>
+        <div class="row row-space justify-content-center">
+
+        <p class="text-uppercase font-weight-bold">Cognome</p>
+
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Email</p>
+   
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Sesso</p>  
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Data di nascita</p>
+
+                
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Numero di telefono</p>
+
+                
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Indirizzo Domicilio</p>
+
+                
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Dottore</p>
+
+                
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Ambulatorio</p>
+
+                
+        </div>
+        <div class="row row-space justify-content-center">
+        <p class="text-uppercase font-weight-bold">Numero di Telefono Dottore</p>
+
+                
+        </div>
+    </div>
+
+            
+    <div class="col-lg-4" align="center">
+        <div class="row row-space justify-content-center">
+            <p class="text-uppercase">{{ $patient->fiscal_code }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $user->name }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $user->surname }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $user->email }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $patient->gender}}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ date('d/m/Y', strtotime($patient->dob)) }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $patient->phone_number }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $patient->street_address }}, {{ $patient->street_number }} - {{ $patient->city }} - {{ $patient->postal_code }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $userDoctor->name }} {{ $userDoctor->surname }}</p>
+        </div>
+        <div class="row row-space justify-content-center"> 
+            <p class="text-uppercase">{{ $building->street_address }}, {{ $building->street_number }} - {{ $building->city }} - {{ $building->postal_code }}</p>
+        </div>
+        <div class="row row-space justify-content-center">    
+            <p class="text-uppercase">{{ $doctor->phone_number }}</p>
+        </div>
+    </div>
+</div>
+</div>
 
  @endif
 @endsection
